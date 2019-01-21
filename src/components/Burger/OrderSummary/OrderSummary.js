54a4772e96f63d667045266200062eaf5eb42b35
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-21 19:45:18 +0000

    Adding a toolbar

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,6 +19,7 @@ const orderSummary = (props) => {
             <h3>Your Order</h3>
             <p>A delicious burguer w/ the following ingredients:</p>
             <ul>{ingredientSummary}</ul>
+            <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
             <p>Continue to checkout</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
