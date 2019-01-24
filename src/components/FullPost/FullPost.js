c1f949b297994eead6066e7e62f336255519fa35
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-24 17:26:04 +0000

    adding if statemants to the componentDidUpdate

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
-
 import classes from './FullPost.module.css';
+import axios from 'axios';
 
 class FullPost extends Component {
-    render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+    state = {
+        loadedPost: null
+    }
+    componentDidUpdate () {
+
         if (this.props.id) {
+            if( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                .then(response => {
+                    this.setState({loadedPost: response.data});
+                });
+            }
+
+
+        }
+
+    }
+    render () {
+        
+        let post = <p style={{textAlign: 'center'}}>Loading..</p>;
+        
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        }
+
+        if (this.state.loadedPost) {
+        
             post = (
                 <div className={classes.FullPost}>
-                    <h1>{this.props.id}</h1>
-                    <p>Content</p>
+                    
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className={classes.Edit}>
                         <button className={classes.Delete}>Delete</button>
                     </div>
