2997d1b0ea0db80f95d7665be5a3050977d64ed8
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-24 14:03:57 +0000

    rendering data from api

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import './Post.module.css';
+import classes from './Post.module.css';
 
 const post = (props) => (
-    <article className="Post">
-        <h1>Title</h1>
-        <div className="Info">
-            <div className="Author">Author</div>
+    <article className={classes.Post}>
+        <h1>{props.title}</h1>
+        <div className={classes.Info}>
+            <div className={classes.Author}>Author</div>
         </div>
     </article>
 );
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,20 +8,26 @@ import axios from 'axios';
 
 
 class Blog extends Component {
+    state = {
+        posts: [ ]
+    }
+
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response => {
+            this.setState({posts: response.data})
             console.log(response);
         });
     }
 
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />
+        })
         return (
             <div>
                 <section className={classes.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
+                   {posts}
                 </section>
                 <section>
                     <FullPost />
