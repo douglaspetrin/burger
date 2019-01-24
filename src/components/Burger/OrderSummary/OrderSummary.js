99d5e270d46f7dddc29f9df8401e96dab29b147e
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-24 11:07:47 +0000

    Improving performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    componentWillUpdate() {
+        console.log('opa');
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    render () {
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map( igKey => {
             return (
             <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
             </li>
             );
         });
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burguer w/ the following ingredients:</p>
-            <ul>{ingredientSummary}</ul>
-            <p>Total Price: <strong>{props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    ); 
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burguer w/ the following ingredients:</p>
+                <ul>{ingredientSummary}</ul>
+                <p>Total Price: <strong>{this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 };
  
-export default orderSummary;
+export default OrderSummary;
