db38373d34d9e759f1dde4d7c425308cf1c58a46
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-27 13:24:06 +0000

    added Switch from React-Router-Dom

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import classes from './Blog.module.css';
 import Posts from '../Blog/Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 
 
 class Blog extends Component {
@@ -31,9 +31,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
-                <Route path="/:id" component={FullPost} />
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts/:id" component={FullPost} />
+                </Switch>
             </div>
         );
     }
