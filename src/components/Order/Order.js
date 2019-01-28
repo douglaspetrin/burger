9fad9a08d845a03cde9698ac75465a4256a95450
Douglas Petrin <douglas@douglaspetrin.com>
2019-01-28 18:55:11 +0000

    up on fetching data to Orders comp

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import classes from './Order.module.css'; 
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>opa</p>
-        <p>opa</p>
-    </div>
-);
+const order = (props) => {
+    const ingredients = [];
+
+    for (let ingredientsName in props.ingredients) {
+        ingredients.push(
+            {
+            name: ingredientsName,
+            amout: props.ingredients[ingredientsName]
+            }
+        );
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+                style={{
+                    textTransform: 'capitalize',
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'
+                    }}
+                key={ig.name}>{ig.name} ({ig.amout}) </span>;
+    });
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>{Number.parseFloat(props.price).toFixed(2)}</strong></p>
+        </div>
+    );
+};
  
 export default order;
