4da568ec7c90833e3ed84afb143fe9e2c52a11b2
Douglas Petrin <douglas@douglaspetrin.com>
2019-02-05 18:23:57 +0000

    guarding routes

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -24,6 +24,9 @@ export const authFail = (error) => {
 
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTH_LOGOUT
     };
@@ -56,6 +59,12 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+
+                const expirationDate = new Date (new Date ().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate); // to store the token
+                localStorage.setItem('userId', response.data.localId);
+
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(response.data.expiresIn));
             })
@@ -71,4 +80,24 @@ export const setAuthRedirectPath = (path) => {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
     }
+};
+
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = new Date (localStorage.getItem('expirationDate')); // wrapped because it returned a string and we need the date object
+            if (expirationDate <= new Date()) {
+                //  if yes we wanna logout
+                dispatch(logout());
+            } else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(checkAuthTimeout( (expirationDate.getTime() - new Date().getTime()) / 1000 ));
+            }
+        }
+    };
 };
