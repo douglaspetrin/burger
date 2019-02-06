850b74fa3cb4499e75c7851e68802b475470a961
Douglas Petrin <douglas@douglaspetrin.com>
2019-02-06 19:45:45 +0000

    put checkValidity in utility to reuse it

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,7 +7,7 @@ import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Auth.module.css'; 
 
-import {updateObject} from '../../shared/utility';
+import {updateObject, checkValidity} from '../../shared/utility';
 
 import * as actions from '../../store/actions/index';
 
@@ -55,46 +55,11 @@ class Auth extends Component {
         }
     }
 
-
-    checkValidity(value, rules) {
-        let isValid = true;
-
-        if (!rules){
-            return true;
-        }
-
-        if (rules.required) {
-            // rules.required means it is true
-            isValid = value.trim() !== '' && isValid; //trim() removes empty spaces, comparing if is not equal to empty string ''
-        }
-
-        if (rules.minLength) {
-            isValid = value.length >= rules.minLength && isValid
-        }
-
-        if (rules.maxLength) {
-            isValid = value.length <= rules.maxLength && isValid
-        }
-
-        if (rules.isEmail) {
-            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-            isValid = pattern.test(value) && isValid
-        }
-
-        if (rules.isNumeric) {
-            const pattern = /^\d+$/;
-            isValid = pattern.test(value) && isValid
-        }
-
-
-        return isValid; // true or false
-    }
-
     inputChangeHandler = (event, controlName) => {
         const updatedControls = updateObject(this.state.controls, {
             [controlName]: updateObject(this.state.controls[controlName], {
                 value: event.target.value,
-                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidity(event.target.value, this.state.controls[controlName].validation),
                 touched: true 
             }) 
         });
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -5,3 +5,37 @@ export const updateObject = (oldObject, updatedProperties) => {
         
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+
+    if (!rules){
+        return true;
+    }
+
+    if (rules.required) {
+        // rules.required means it is true
+        isValid = value.trim() !== '' && isValid; //trim() removes empty spaces, comparing if is not equal to empty string ''
+    }
+
+    if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid
+    }
+
+    if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid
+    }
+
+    if (rules.isEmail) {
+        const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+        isValid = pattern.test(value) && isValid
+    }
+
+    if (rules.isNumeric) {
+        const pattern = /^\d+$/;
+        isValid = pattern.test(value) && isValid
+    }
+
+
+    return isValid; // true or false
+}
